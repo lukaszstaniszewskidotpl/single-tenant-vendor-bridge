cc8399851998cc8e95f33ee6fd5c76892a3c120b
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2023-05-12 23:12:33 +0200

    add compiller pass for controller

---
+++ src/CompilerPass/OverrideControllerCompilerPass.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ferdyrurka\SingleTenantVendorBridge\CompilerPass;
+
+use Ferdyrurka\SingleTenantVendorBridge\Controller\TransportController;
+use Ferdyrurka\SingleTenantVendor\Controller\TransportController as BaseTransportController;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class OverrideControllerCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        $container->setDefinition(
+            BaseTransportController::class,
+            $container->getDefinition(TransportController::class),
+        );
+    }
+}
--- src/SingleTenantVendorBridgeBundle.php
+++ src/SingleTenantVendorBridgeBundle.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 
 namespace Ferdyrurka\SingleTenantVendorBridge;
 
+use Ferdyrurka\SingleTenantVendorBridge\CompilerPass\OverrideControllerCompilerPass;
 use Ferdyrurka\SingleTenantVendorBridge\DependencyInjection\SingleTenantVendorBridgeExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SingleTenantVendorBridgeBundle extends AbstractBundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new OverrideControllerCompilerPass());
+    }
+
     public function getContainerExtension(): ExtensionInterface
     {
         return new SingleTenantVendorBridgeExtension();
